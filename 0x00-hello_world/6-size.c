1f842954d084324acb4eb98fef03cbcfe592a2ef
lilHommie <littlehommie9@gmail.com>
2022-12-09 08:44:33 -0800

    my second line of c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -print out sizes of data types in c
+ * code by Hommie
+ * Return: 0
+*/
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	print("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	print("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	print("Size of a long int: %lu byte(s)\n" (unsigned long)sizeof(c));	
+	print("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	print("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
