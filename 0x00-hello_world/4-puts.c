ddec23cd86b40ce488ffd719ab8f14772bc7aa2a
lilHommie <littlehommie9@gmail.com>
2022-12-09 01:27:34 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+/*
+ * main - prints a line of code using puts
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"):
+	return (0):
+}
+
