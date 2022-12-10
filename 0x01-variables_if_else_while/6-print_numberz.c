3fed100c51f0ea044cda466c882792945b9a7974
lilHommie <littlehommie9@gmail.com>
2022-12-10 07:12:37 -0800

    task 5

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print all single nunber from 0 to 9
+ * Return: 0
+*/
+
+int main(void)
+{
+	char num;
+
+	for (num = 0; num <= 9; num++)
+		putchar(num);
+
+	putchar("\n");
+
+	return (0);
+}
