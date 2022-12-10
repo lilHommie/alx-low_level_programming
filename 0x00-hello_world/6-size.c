7f8ea57925857e312381c5b9a0e2dc2e191842eb
lilHommie <littlehommie9@gmail.com>
2022-12-09 21:24:01 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 
 	print("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	print("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	print("Size of a long int: %lu byte(s)\n" (unsigned long)sizeof(c));	
+	print("Size of a long int: %lu byte(s)\n" (unsigned long)sizeof(c));
 	print("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	print("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
