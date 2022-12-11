7f345116f559d7750c19b1f229051ed30c8be2da
lilHommie <littlehommie9@gmail.com>
2022-12-11 03:25:14 -0800

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,23 +13,27 @@ int main(void)
 	int hundreds = '0';
 
 	for (hundreds = '0'; hundreds <= '9'; hundreds++)
-
-	for (tens = '0'; tens <= '9'; tens++)
-
-	for (ones = '0'; ones <= '9'; ones++)
-
-	if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
 	{
-		putchar(hundreds);
-		putchar(tens);
-		putchar(ones);
+		for (tens = '0'; tens <= '9'; tens++)
+		{
+			for (ones = '0'; ones <= '9'; ones++)
+			{
+				if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
+				{
+					putchar(hundreds);
+					putchar(tens);
+					putchar(ones);
+					if (!(ones == '9' && hundreds == '7' && tens == '8')
+										{
+										putchar(',');
+										putchar(' ');
+										}
+									}
+								}
+							}
 
-	if (!(ones == '9' && hundreds == '7' && tens == '8')
-			putchar(',');
-			putchar(' ');
-	}
+
 }
-
 	putchar('\n');
 
 	return (0);
