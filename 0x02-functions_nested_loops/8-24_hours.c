4e2e9870290629c57ef3a2f06bfa9db6a167d357
lilHommie <littlehommie9@gmail.com>
2022-12-14 08:01:10 -0800

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,12 +8,28 @@ void jack_bauer(void)
 {
 	int hour, minute;
 
-	for (hour = 0; hour <= 23; hour++)
+	for (hour = 0; hour < 24; hour++)
 	{
-		for (minute = 0; minute <= 59; minute++)
+		for (minute = 0; minute < 60; minute++)
+		{
+		if (hour < 10)
+		{
+			_putchar('0');
+			_putchar(hour + '0');
+		}
+		else if (hour >= 10)
 		{
 			_putchar((hour / 10) + 0);
 			_putchar((hour % 10) + 0);
+		}
+		if (minutes < 10)
+		{
+			_putchar(':');
+			_putchar('0');
+			_putchar(minutes + '0');
+		}
+		else if (minutes > 10)
+		{
 			_putchar(':');
 			_putchar((minute / 10) + 0);
 			_putchar((minute % 10) + 0);
