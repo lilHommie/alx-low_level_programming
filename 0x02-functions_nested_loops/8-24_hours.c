8a17a25234444418490ed42e44e05199213b3707
lilHommie <littlehommie9@gmail.com>
2022-12-14 08:27:26 -0800

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,29 +12,29 @@ void jack_bauer(void)
 	{
 		for (minute = 0; minute < 60; minute++)
 		{
-		if (hour < 10)
-		{
-			_putchar('0');
-			_putchar(hour + '0');
-		}
-		else if (hour >= 10)
-		{
-			_putchar((hour / 10) + 0);
-			_putchar((hour % 10) + 0);
-		}
-		if (minute < 10)
-		{
-			_putchar(':');
-			_putchar('0');
-			_putchar(minute + '0');
-		}
-		else if (minute >= 10)
-		{
-			_putchar(':');
-			_putchar((minute / 10) + 0);
-			_putchar((minute % 10) + 0);
-		}
-		_putchar('\n');
+			if (hour < 10)
+			{
+				_putchar('0');
+				_putchar(hour + '0');
+			}
+			else if (hour >= 10)
+			{
+				_putchar((hour / 10) + 0);
+				_putchar((hour % 10) + 0);
+			}
+			if (minute < 10)
+			{
+				_putchar(':');
+				_putchar('0');
+				_putchar(minute + '0');
+			}
+			else if (minute >= 10)
+			{
+				_putchar(':');
+				_putchar((minute / 10) + 0);
+				_putchar((minute % 10) + 0);
+			}
+			_putchar('\n');
 		}
 	}
 }
