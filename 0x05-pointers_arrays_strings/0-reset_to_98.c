43db3239690515e6dabf43d42eda226cef5ac6b6
lilHommie <littlehommie9@gmail.com>
2022-12-19 06:29:59 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - to reset the value of n from 402 to 98.
+  * @n: will be converted to a pointer "*n" in such manner.
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
