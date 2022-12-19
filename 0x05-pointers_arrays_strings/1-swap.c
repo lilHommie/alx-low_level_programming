572d1df4185d2578ea99454388bfa005271698d2
lilHommie <littlehommie9@gmail.com>
2022-12-19 06:32:58 -0800

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+
+#include "main.h"
+
+/**
+  * swap_int - To swap the value of a and b from 98, 42.
+  * @a: The value of 'a' will be swaped to 42 while.
+  * @b: The value of 'b' will be swaped to 98.
+  */
+
+void swap_int(int *a, int *b)
+{
+	*a = *a + *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}
